fddb98f3344306b529650af8c109a75c5b214ae4
nannnnn1 <1329876248@qq.com>
2026-06-10 23:27:41 +0800

    fix(api): increase JSON body parser limit to 10MB for parse callback payloads

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory, Reflector } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
@@ -6,7 +7,9 @@ import { TransformInterceptor } from "./common/interceptors/transform.intercepto
 import { AllExceptionsFilter } from "./common/filters/all-exceptions.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useBodyParser("json", { limit: "10mb" });
 
   app.useGlobalPipes(
     new ValidationPipe({
